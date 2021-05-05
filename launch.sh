e1bae1a4c7b4ac17afa42eddba4cc2b05928e9bb
root <jorrgebustos@gmail.com>
2021-05-05 15:29:09 +0000

    baseline df server works

--- launch.sh
+++ launch.sh
@@ -1,1 +1,3 @@
-docker build - < scripts/Dockerfile
+docker build -t dfsv_image .
+docker run -d -t --name mixed_1 -p 27960:27960/udp -p 27960:27960/tcp dfsv_image:latest
+docker cp cfgs/q3config_server.cfg mixed_1:/dfsv/defrag/
--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,4 @@ echo 'sets .mapbase "$SV_MAPBASE"' >> defrag/q3config_server.cfg
 echo 'sets .server-location "$SV_LOCATION"' >> defrag/q3config_server.cfg
 echo 'seta rconPassword "$SV_RCON"' >> defrag/q3config_server.cfg
 echo 'seta sv_hostname "$SV_HOSTNAME"' >> defrag/q3config_server.cfg
-/dfsv/oDFe.ded +set fs_homepath /dfsv +set net_enabled +set net_port 27960 +set rs_server_id $RS_SV_ID +set com_hunkmegs 256 +set sv_pure 0 +set sv_levelTimeReset 1 +set fs_game defrag +set dedicated 2 +set vm_game 0 +set ttycon_ansicolor 1 +set bot_enable 0 +exec idle.cfg
+/dfsv/oDFe.ded.x64 +set fs_homepath . +set net_enabled 1 +set net_port 27960 +set rs_server_id $RS_SV_ID +set com_hunkmegs 256 +set sv_pure 0 +set sv_levelTimeReset 1 +set fs_game defrag +set dedicated 2 +set vm_game 0 +set ttycon_ansicolor 1 +set bot_enable 0 +exec cfgs/mixed.cfg
