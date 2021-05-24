e78e4346ead4cdbe5a70106a12b7b7572babc238
root <jorrgebustos@gmail.com>
2021-05-24 03:34:50 +0000

    starting point

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,5 @@
 FROM ubuntu:bionic
 
-RUN apt-get update && apt-get -y install unzip wget
-
-# get q3 server backbone
-RUN wget http://dl.defrag.racing/downloads/dfsv.tar.gz
-RUN tar -xvzf dfsv.tar.gz
-
 WORKDIR /dfsv
-
-# get latest version of dedicated engine
-RUN wget https://github.com/JBustos22/oDFe/releases/download/latest/oDFe-linux-x86_64.zip
-RUN unzip oDFe-linux-x86_64.zip && rm oDFe.x64 oDFe-linux-x86_64.zip oDFe.vk.x64
-
-# get latest version of DF
-RUN wget $(cd /tmp/ && wget --spider -r --no-parent --no-check-certificate https://q3defrag.org/files/defrag/ 2>&1 | grep -E "\-\-2" | grep "defrag_" | grep -v "beta" | cut -d' ' -f4 | sort | tail -n1)
-RUN unzip -o defrag*.zip && rm *.zip
-
-COPY ./cfgs/ ./defrag/cfgs/
-COPY ./scripts/start.sh .
-RUN chmod +x oDFe.ded.x64
 
 ENTRYPOINT ./start.sh
--- launch.sh
+++ launch.sh
@@ -1,3 +1,24 @@
-docker build -t dfsv_image .
-docker run -d -t --name mixed_1 -p 27960:27960/udp -p 27960:27960/tcp dfsv_image:latest
-docker cp cfgs/q3config_server.cfg mixed_1:/dfsv/defrag/
+SV_HOSTNAME="Uncle Sam's Defrag ATL"
+SV_RCON="unclesam"
+SV_LOCATION="Atlanta, Georgia"
+MIXED_COUNT=
+
+#OPTIONAL
+ADMIN_NAME=""
+ADMIN_MAIL=""
+ADMIN_JABBER=""
+ADMIN_IRC=""
+SV_MAPBASE=""
+SV_HOMEPAGE=""
+
+COUNTER=0
+for CONFIGURABLE in SV_HOSTNAME SV_RCON SV_LOCATION; do
+	if [[ "${!CONFIGURABLE}" = "" ]]
+	then
+		read -p "Enter $CONFIGURABLE: " $CONFIGURABLE
+	fi
+done
+printf "\nServer Hostname: $SV_HOSTNAME\nRcon Password: $SV_RCON\nServer Location: $SV_LOCATION\n\n"
+
+docker run -d -t --name mixed_1 -v dfsv:/dfsv -p 27960:27960/udp -p 27960:27960/tcp dfsv_image:latest
+docker cp servers/base/defrag/cfgs/q3config_server.cfg mixed_1:/dfsv/defrag/
