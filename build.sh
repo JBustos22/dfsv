43d5f60e0938e147a0d9e2340983df2bc30783cd
root <jorrgebustos@gmail.com>
2021-05-26 21:23:06 +0000

    Fixed configs, added docker installation

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
+apt update && sudo apt upgrade
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get install docker-ce docker-ce-cli containerd.io
+
 base=./servers/base/
 apt-get update && apt-get install unzip wget
 # set up server backbone
